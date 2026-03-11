a6f6c74406d95bb1732b4635e5341459a22a4f06
Gizachew <gizachewkassa33@gmail.com>
2026-03-11 17:11:31 +0000

    Onclick

--- my-firstExercise/src/App.jsx
+++ my-firstExercise/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 //  import UserCard from './UserCard.jsx'
-import Profile from './components/Profile.jsx'
+// import Profile from './components/Profile.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   return (
     <div>
       {/* <h1>WELLOCOM TO REACT TRAINING </h1>
@@ -13,18 +13,33 @@ function App() {
        {/* <UserCard name="Gizachew " course="Full Stack Development" />
        <UserCard name="Eliyas" course="React Training" /> */} 
       
-     <h1>Counter Example</h1>
+     {/* <h1>Counter Example</h1>
         <p>Count: {count}</p>
         <button onClick={() => setCount(count + 1)}>
           Increase
           </button>
       <h2>Profile Component</h2>
    <Profile
-   name="Gizachew Kassa"
-   email="gizachewkassa33@example.com"
+   name="Samuel"
+   email="samuel@gmail.com"
    role="MERN Developer"
    />
-    </div>
+
+    <Profile
+   name="Gizachew Kassa"
+   email="gizachewkassa33@gmail.com"
+   role="MERN Developer"
+   />
+    <Profile
+   name="Yohhanes"
+   email="yohaness@gmail.com"
+   role="NET Developer"
+   /> */}
+
+   <h1 id="hello">hello dear student</h1>
+    <button onClick={() => {
+        document.getElementById('hello').innerText = "Welcome to MERN training"}}>click me</button>    
+        </div>
   );
 }
 
