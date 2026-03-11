848637201b5e14de33916e839711e78146bd5207
Gizachew <gizachewkassa33@gmail.com>
2026-03-11 15:56:27 +0000

    apdate app.jsx

---
+++ my-firstExercise/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <h1>wELLOCOM TO REACT TRAINING </h1>
+      <p>this is my react app</p>
+     
+    </div>
+  );
+}
+
+export default App
