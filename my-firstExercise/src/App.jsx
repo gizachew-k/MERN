89543b3cc2b64eeba50f822a84ea41bf091af154
Gizachew <gizachewkassa33@gmail.com>
2026-03-11 16:28:07 +0000

    props,states in react

--- my-firstExercise/src/App.jsx
+++ my-firstExercise/src/App.jsx
@@ -5,13 +5,21 @@ import UserCard from './UserCard'
 import './App.css'
 
 function App() {
-  
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>WELLOCOM TO REACT TRAINING </h1>
+      {/* <h1>WELLOCOM TO REACT TRAINING </h1>
       {/* <UserCard /> */}
-      <UserCard name="Gizachew " course="Full Stack Development" />
-       <UserCard name="Eliyas" course="React Training" />
+      {/* passing props to the user card component? */}
+       {/* <UserCard name="Gizachew " course="Full Stack Development" />
+       <UserCard name="Eliyas" course="React Training" /> */} 
+
+
+      <h1>Counter Example</h1>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>
+          Increase
+          </button>
     </div>
   );
 }
--- my-firstExercise/src/UserCard.jsx
+++ my-firstExercise/src/UserCard.jsx
@@ -8,9 +8,13 @@ function UserCard(props) {
 
 <div>
  <h2>Student Information</h2>
+ {/* recieving props from the parent component (App.jsx) and displaying it here in the user card component */}
  <p>Name: {props.name}</p>
  <p>Course: {props.course}</p>
  </div>
  );
 }
 export default UserCard;
+
+
+
