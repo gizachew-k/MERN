9743415f527bc528321042fa5f58b60381a9b6fd
Gizachew <gizachewkassa33@gmail.com>
2026-03-15 12:40:51 +0000

    usestate

--- my-firstExercise/src/App.jsx
+++ my-firstExercise/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 //  import UserCard from './UserCard.jsx'
 // import Profile from './components/Profile.jsx'
-import Counter from './components/Counter.jsx'
-// import TodoList from './components/TodoList.jsx'
+import Counter from './components/pages/Counter.jsx'
 import './App.css'
 import Header from './components/pages/Header.jsx';
 import NavBar from './components/pages/NavBar.jsx';
